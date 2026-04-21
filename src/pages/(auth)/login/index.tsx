0be9be0899c84fd845ccb3f1d9e8c2e96756c136
sultani-ali81 <alisultani.373@gmail.com>
2026-04-21 15:38:59 +0430

    Refactor Navbar import and usage in Login component

--- src/pages/(auth)/login/index.tsx
+++ src/pages/(auth)/login/index.tsx
@@ -1,19 +1,14 @@
-import LoginForm from "@/components/layout/login-form";
-//import Navbar from "@/components/layout/navbar";
-import StatsPanel from "@/components/layout/stats-panel";
 import AuthLayout from "@/components/layout/auth-layout";
+import LoginForm from "@/components/layout/login-form";
+import Navbar from "@/components/layout/navbar";
+import StatsPanel from "@/components/layout/stats-panel";
 
 export default function Login() {
   return (
     <>
-      {/* <div className="md:hidden">
-        <Navbar
-          userName="John Doe"
-          avatarUrl="/photos/profile.jpeg"
-          onMenuClick={() => console.log("menu")}
-        />
-      </div> */}
-
+      <div className="md:hidden">
+        <Navbar userName="John Doe" onMenuClick={() => console.log("menu")} />
+      </div>
 
       <AuthLayout left={<LoginForm />} right={<StatsPanel />} />
     </>
