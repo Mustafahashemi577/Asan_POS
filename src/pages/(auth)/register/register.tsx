8f7a5be5a0cce4b7a20ea7cbde2fdee09f2db6d1
sultani-ali81 <alisultani.373@gmail.com>
2026-04-21 15:38:06 +0430

    Uncomment Navbar component in Register page

--- src/pages/(auth)/register/register.tsx
+++ src/pages/(auth)/register/register.tsx
@@ -1,18 +1,14 @@
 import AuthLayout from "@/components/layout/auth-layout";
-//import Navbar from "@/components/layout/navbar";
+import Navbar from "@/components/layout/navbar";
 import RegisterForm from "@/components/layout/register-form";
 import StatsPanel from "@/components/layout/stats-panel";
 
 export default function Register() {
   return (
     <>
-      {/* <div className="md:hidden">
-        <Navbar
-          userName="John Doe"
-          avatarUrl="/photos/profile.jpeg"
-          onMenuClick={() => console.log("menu")}
-        />
-      </div> */}
+      <div className="md:hidden">
+        <Navbar userName="John Doe" onMenuClick={() => console.log("menu")} />
+      </div>
 
       <AuthLayout left={<RegisterForm />} right={<StatsPanel />} />
     </>
